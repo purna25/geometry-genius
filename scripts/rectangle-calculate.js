8f566a2b85d860b430d3e93347762ccbce0af976
purna25 <cpurnasree18@gmail.com>
2024-12-11 10:48:48 +0600

    added rectangle container

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -1,3 +1,21 @@
 function calculateRectangleArea(){
-  console.log('inside')
+  // get width of the rectangle
+  const widthInput = document.getElementById('rectangle-width');
+  const widthText = widthInput.value;
+  const width = parseFloat(widthText);
+  console.log(width);
+
+  // get length of the rectangle
+  const lengthInput = document.getElementById('rectangle-length');
+  const lengthText = lengthInput.value;
+  const length = parseFloat(lengthText);
+  console.log(length);
+
+  // calculate rectangle area
+  const area = width * length;
+  console.log('area of the rectangle:', area);
+
+  // display rectangle area
+  const rectangleAreaSpan = document.getElementById('rectangle-area');
+  rectangleAreaSpan.innerText = area;
 }
